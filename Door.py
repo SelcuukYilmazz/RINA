6ee60479ee09fff18bb980f4b5fb8be692323e52
Selçuk Yılmaz <selcukyilmaz2000@hotmail.com>
2021-02-15 20:56:42 +0300

    Add files via upload

---
+++ Door.py
@@ -0,0 +1,7 @@
+class Door:
+    lock = False
+    corners = 0
+    area = 0
+    time = 0
+    box = []
+    center = []
--- process_image.py
+++ process_image.py
@@ -1,42 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
-import time
+from Door import Door
 
 
 #######################################################################################################################
 #  Functions
     # Getting Contours Of Image
-def getContours(img,imgContour,relative_area,box,corners,time):
-    print(relative_area)
+def getContours(img,imgContour,door):
     contours,hierarchy = cv.findContours(img,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         area = cv.contourArea(cnt)
-        if area >= relative_area:
+
+        # If area getting bigger then lock to that area and release this but if it is a rectangle
+        if area >= door.area:
+
             # Reduce Mistakes With Approximation Functions
-            time = 0
             peri = cv.arcLength(cnt, True)
             approx = cv.approxPolyDP(cnt, 0.025 * peri, True)
             if len(approx) == 4:
-                relative_area = area
+
+                # Reset time everytime code gets in here
+                door.time = 0
+                door.area = area
                 rect = cv.minAreaRect(cnt)
-                box = cv.boxPoints(rect)
-                box = np.int0(box)
-                corners = len(approx)
+                door.box = cv.boxPoints(rect)
+                door.box = np.int0(door.box)
+                door.corners = len(approx)
+                door.lock = True
 
-    if time == 20:
-        box = []
-        relative_area = 186
+    # Reset every 60 repeats
+    if door.time == 60:
+        door.box = []
+        door.area = 186
+        door.center = []
+        door.lock = False
 
-    if len(box) != 0:
-        center = (box[0] + box[2]) // 2
-        cv.drawContours(imgContour, [box], -1, (255, 0, 255), 7)
-        cv.circle(imgContour,(center[0],center[1]), 0, (0,255,0), 15)
-        cv.putText(imgContour,'Points: ' + str(corners),(center[0] + 20,center[1] + 20), cv.FONT_HERSHEY_DUPLEX,.7,(0,255,0),2)
-        cv.putText(imgContour,'Area: ' + str(int(relative_area)), (center[0] + 20,center[1] + 45),cv.FONT_HERSHEY_DUPLEX,0.7,(0,255,0),2)
-        cv.putText(imgContour,'X_Axis: '+str(center[0]),((center[0] + 20,center[1] + 70)),cv.FONT_HERSHEY_DUPLEX,0.7,(0,255,0),2)
-        cv.putText(imgContour,'Y_Axis: ' + str(center[1]), ((center[0] + 20, center[1] + 95)),cv.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 0), 2)
-    return relative_area,box,corners,time
+    # If door locked on something then draw it
+    if door.lock:
+        door.center = (door.box[0] + door.box[2]) // 2
+        cv.drawContours(imgContour, [door.box], -1, (255, 0, 255), 7)
+        cv.circle(imgContour,(door.center[0],door.center[1]), 0, (0,255,0), 15)
+        cv.putText(imgContour,'Points: ' + str(door.corners),(door.center[0] + 20,door.center[1] + 20), cv.FONT_HERSHEY_DUPLEX,.7,(0,255,0),2)
+        cv.putText(imgContour,'Area: ' + str(int(door.area)), (door.center[0] + 20,door.center[1] + 45),cv.FONT_HERSHEY_DUPLEX,0.7,(0,255,0),2)
+        cv.putText(imgContour,'X_Axis: '+str(door.center[0]),((door.center[0] + 20,door.center[1] + 70)),cv.FONT_HERSHEY_DUPLEX,0.7,(0,255,0),2)
+        cv.putText(imgContour,'Y_Axis: ' + str(door.center[1]), ((door.center[0] + 20, door.center[1] + 95)),cv.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 0), 2)
+    return
 
 
     # Whenever Trackbar Moves This Function Will Be Executed
@@ -49,37 +58,30 @@ def stackImages(scale,imgArray):
     cols = len(imgArray[0])
 
     # Checking Data Type
-
     rowsAvailable = isinstance(imgArray[0],list)
 
     # Taking Variables
-
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
 
     # If Data is List Then Go
-
     if rowsAvailable:
         for x in range (0,rows):
             for y in range (0,cols):
 
                 # If Image Width and Height Is Equals To First Image Then Resize Both Image
-
                 if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
                     imgArray[x][y] = cv.resize(imgArray[x][y],(0,0),None,scale,scale)
 
                 # If Image Width and Height Is Not Equals To First Image Then Resize Both Image But Different Ratios
-
                 else:
                     imgArray[x][y] = cv.resize(imgArray[x][y],(imgArray[0][0].shape[1],imgArray[0][0].shape[0]),None,scale,scale)
 
                 # If Image is Grey Filtered Then Make It BGR
-
                 if len(imgArray[x][y].shape) == 2:
                     imgArray[x][y] = cv.cvtColor(imgArray[x][y],cv.COLOR_GRAY2BGR)
 
         # Create Empty Window Then Open Screens On It
-
         imageBlank = np.zeros((height,width,3),np.uint8)
         hor = [imageBlank]*rows
         for x in range(0,rows):
@@ -87,7 +89,6 @@ def stackImages(scale,imgArray):
         ver = np.vstack(hor)
 
     # If Data Is Not List Then Take Pictures One By One And Make All Functions Above As The Same
-
     else:
         for x in range(0,rows):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
@@ -102,7 +103,7 @@ def stackImages(scale,imgArray):
 
 
 # Image Process Main Function
-def image_process(capture, relative_area,box,corners,time):
+def image_process(capture,door):
     # If Camera Captures Video Than isTrue is True
     # frame Is Captured Video
     isTrue, frame = capture.read()
@@ -125,6 +126,7 @@ def image_process(capture, relative_area,box,corners,time):
 
     # Original Image to HSV
     imgHsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
     # Color Detection Numpy Matrix
     lower = np.array([hueMin, satMin, valMin])
     upper = np.array([hueMax, satMax, valMax])
@@ -135,15 +137,12 @@ def image_process(capture, relative_area,box,corners,time):
     imgContour = frame.copy()
 
     # Blur
-
     blur = cv.GaussianBlur(frame, (7, 7), 1)
 
     # Grey Filter
-
     grey = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
 
     # Canny Edge Detector
-
     canny = cv.Canny(grey, threshold1, threshold2)
 
     # Dilation Function This Function Makes Bright Pixels Brighter And Program Can See Edges More Clearly With This
@@ -151,30 +150,16 @@ def image_process(capture, relative_area,box,corners,time):
     imgDil = cv.dilate(canny, kernel, iterations=4)
 
     # Erode Function This Function Makes Photo Little Smaller
-
     imgDil = cv.erode(imgDil, kernel, iterations=2)
 
     # Get Contours
-    relative_area,box,corners,time = getContours(imgDil, imgContour,relative_area,box,corners,time)
+    getContours(imgDil, imgContour,door)
 
     # Stack Images
     imgStack = stackImages(0.8, ([frame, canny, mask], [imgDil, imgContour, result]))
     cv.imshow('All', imgStack)
-    return relative_area,box,corners,time
+    return
 
-
-
-
-########################################################################################################################
-########################################################################################################################
-############################################# Main Code ######################################
-# Variables
-relative_area = 186
-box = []
-corners = 0
-time = 0
-# Capturing Video From Your Camera
-capture = cv.VideoCapture(0)
 
 # Trackbar Interface
 cv.namedWindow('Parameters')
@@ -187,9 +172,19 @@ cv.createTrackbar('SAT Min', 'Parameters', 73, 255, empty)
 cv.createTrackbar('SAT Max', 'Parameters', 173, 255, empty)
 cv.createTrackbar('VALUE Min', 'Parameters', 0, 255, empty)
 cv.createTrackbar('VALUE Max', 'Parameters', 255, 255, empty)
+
+########################################################################################################################
+########################################################################################################################
+############################################# Main Code ######################################
+# Variables
+door = Door()
+
+# Capturing Video From Your Camera
+capture = cv.VideoCapture(0)
+
 while True:
-    relative_area,box,corners,time = image_process(capture,relative_area,box,corners,time)
-    time += 1
+    image_process(capture,door)
+    door.time += 1
     # Wait until you press d
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
